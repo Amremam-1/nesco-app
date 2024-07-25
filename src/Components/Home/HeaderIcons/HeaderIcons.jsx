d59250635dad291ed4aa12c7ede94b1d2620e32c
Amr Imam <emama2337@gmail.com>
2024-07-25 16:51:28 +0300

    add menu icon

--- src/Components/Home/HeaderIcons/HeaderIcons.jsx
+++ src/Components/Home/HeaderIcons/HeaderIcons.jsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 import { FiHeart, FiShoppingCart } from "react-icons/fi"
 import { GoPerson } from "react-icons/go"
-import styles from "./styles.module.scss";
-import { useTranslation } from "react-i18next";
-import useLogout from "../../../hooks/auth/useLogout";
-import Cookies from "js-cookie";
-import Popup from "../../Utils/Popup/Popup";
-import useLogin from "../../../hooks/auth/useLogin";
-import useGetCartItems from "../../../hooks/cart/useGetCartItems";
-import { ToastContainer } from "react-toastify";
+import styles from "./styles.module.scss"
+import { useTranslation } from "react-i18next"
+import useLogout from "../../../hooks/auth/useLogout"
+import Cookies from "js-cookie"
+import Popup from "../../Utils/Popup/Popup"
+import useLogin from "../../../hooks/auth/useLogin"
+import useGetCartItems from "../../../hooks/cart/useGetCartItems"
+import { ToastContainer } from "react-toastify"
 
 const HeaderIcons = ({ customHeader }) => {
-  const { t, i18n } = useTranslation();
-  const apiToken = Cookies.get("api_token");
-  const [, , , , , , , , , , isLoggedIn, ] = useLogin();
+  const { t, i18n } = useTranslation()
+  const apiToken = Cookies.get("api_token")
+  const [, , , , , , , , , , isLoggedIn] = useLogin()
   console.log("isLoggedIn:==--==:", isLoggedIn)
-  const [logOutHandler, errMsgPopUp, successMsgPopUp] = useLogout();
+  const [logOutHandler, errMsgPopUp, successMsgPopUp] = useLogout()
   const [
     productsInCartArray,
     cartItemsData,
@@ -25,35 +25,32 @@ const HeaderIcons = ({ customHeader }) => {
     shippingAddress,
     city,
     isLoadingCart,
-    errorCart
-  ] = useGetCartItems();
+    errorCart,
+  ] = useGetCartItems()
 
-  useEffect(() => {
+  useEffect(() => {}, [cartLength])
 
-  }, [cartLength]);
-
-
-  const [lang, setLang] = useState("AR");
+  const [lang, setLang] = useState("AR")
   // const [lang, setLang] = useState(localStorage.getItem("AR"));
-  const [showLangMenu, setShowLangMenu] = useState(false);
-  const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [showLangMenu, setShowLangMenu] = useState(false)
+  const [showAccountMenu, setShowAccountMenu] = useState(false)
 
   const showAccountMenuHandler = () => {
-    setShowAccountMenu(!showAccountMenu);
-    setShowLangMenu(false);
+    setShowAccountMenu(!showAccountMenu)
+    setShowLangMenu(false)
   }
 
   const chooseLangHandler = (selectedLang) => {
-    setLang(selectedLang);
-    i18n.changeLanguage(selectedLang);
+    setLang(selectedLang)
+    i18n.changeLanguage(selectedLang)
     localStorage.setItem("lang", selectedLang)
     // Cookies.set("lang", selectedLang)
-    setShowLangMenu(false);
+    setShowLangMenu(false)
   }
 
   const showLangHandler = () => {
-    setShowLangMenu(!showLangMenu);
-    setShowAccountMenu(false);
+    setShowLangMenu(!showLangMenu)
+    setShowAccountMenu(false)
   }
 
   // const logOutHandler = () => {
@@ -62,12 +59,15 @@ const HeaderIcons = ({ customHeader }) => {
   // }
 
   useEffect(() => {
-    if (localStorage.getItem("i18nextLng") === 'en' || localStorage.getItem("i18nextLng") === 'ar') {
-      setLang(localStorage.getItem("i18nextLng"));
+    if (
+      localStorage.getItem("i18nextLng") === "en" ||
+      localStorage.getItem("i18nextLng") === "ar"
+    ) {
+      setLang(localStorage.getItem("i18nextLng"))
       // setLang(Cookies.get("i18nextLng"));
     } else {
-      setLang("ar");
-      localStorage.setItem("i18nextLng", 'ar')
+      setLang("ar")
+      localStorage.setItem("i18nextLng", "ar")
     }
   }, [lang])
 
@@ -90,14 +90,11 @@ const HeaderIcons = ({ customHeader }) => {
       </> */}
       <a href="/cart" className={styles.cart}>
         <FiShoppingCart />
-        {
-          apiToken ? (
-            <span className={styles.itemsNum}>{cartLength}</span>
-          ): (
-            <span className={styles.itemsNum}>0</span>
-            
-          )
-        }
+        {apiToken ? (
+          <span className={styles.itemsNum}>{cartLength}</span>
+        ) : (
+          <span className={styles.itemsNum}>0</span>
+        )}
       </a>
       {apiToken ? (
         <a href="/store/account/wishlist" className={styles.wishlist}>
@@ -106,58 +103,113 @@ const HeaderIcons = ({ customHeader }) => {
       ) : null}
       <div className={styles.account}>
         <GoPerson onClick={showAccountMenuHandler} />
-        {
-          showAccountMenu ? (
-            <ul className={styles.accountMenu}>
-              {
-                apiToken ? (
-                  <>
-                    <li className={styles.accountMenu__item} onClick={()=>setShowAccountMenu(false)}>
-                      <a className={styles.link} href={process.env.PUBLIC_URL + "/store/account"}>{t("home-header-accountSettings")}</a>
-                    </li>
-                    <li className={styles.accountMenu__item} onClick={()=>setShowAccountMenu(false)}>
-                      <a className={styles.link} href={process.env.PUBLIC_URL + "/store/account/orders"}>{t("home-header-myOrders")}</a>
-                    </li>
-                    <li className={styles.accountMenu__item} onClick={()=>setShowAccountMenu(false)}>
-                      <a className={styles.link} href={process.env.PUBLIC_URL + "/track-order"}>{t("home-header-trackOrder")}</a>
-                    </li>
-                    <li className={styles.accountMenu__item} onClick={() => {
-                      setShowAccountMenu(false);
-                      logOutHandler();
-                    }}
-                    >
-                      <button className={styles.link}>{t("home-header-logout")}</button>
-                    </li>
-                  </>
-                ): (
-                    <>
-                      <li className={styles.accountMenu__item} onClick={()=>setShowAccountMenu(false)}>
-                        <a className={styles.link} href={process.env.PUBLIC_URL + "/login"}>{t("home-header-login")}</a>
-                      </li>
-                      <li className={styles.accountMenu__item} onClick={()=>setShowAccountMenu(false)}>
-                        <a className={styles.link} href={process.env.PUBLIC_URL + "/register"}>{t("home-header-register")}</a>
-                      </li>
-                      <li className={styles.accountMenu__item} onClick={()=>setShowAccountMenu(false)}>
-                        <a className={styles.link} href={process.env.PUBLIC_URL + "/track-order"}>{t("home-header-trackOrder")}</a>
-                      </li>
-                  </>
-                )
-              }
-
-            </ul>
-          ) : null
-        }
+        {showAccountMenu ? (
+          <ul
+            className={
+              i18n.language === "en" ? styles.accountMenu : styles.accountMenuAr
+            }
+          >
+            {apiToken ? (
+              <>
+                <li
+                  className={styles.accountMenu__item}
+                  onClick={() => setShowAccountMenu(false)}
+                >
+                  <a
+                    className={styles.link}
+                    href={process.env.PUBLIC_URL + "/store/account"}
+                  >
+                    {t("home-header-accountSettings")}
+                  </a>
+                </li>
+                <li
+                  className={styles.accountMenu__item}
+                  onClick={() => setShowAccountMenu(false)}
+                >
+                  <a
+                    className={styles.link}
+                    href={process.env.PUBLIC_URL + "/store/account/orders"}
+                  >
+                    {t("home-header-myOrders")}
+                  </a>
+                </li>
+                <li
+                  className={styles.accountMenu__item}
+                  onClick={() => setShowAccountMenu(false)}
+                >
+                  <a
+                    className={styles.link}
+                    href={process.env.PUBLIC_URL + "/track-order"}
+                  >
+                    {t("home-header-trackOrder")}
+                  </a>
+                </li>
+                <li
+                  className={styles.accountMenu__item}
+                  onClick={() => {
+                    setShowAccountMenu(false)
+                    logOutHandler()
+                  }}
+                >
+                  <button className={styles.link}>
+                    {t("home-header-logout")}
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li
+                  className={styles.accountMenu__item}
+                  onClick={() => setShowAccountMenu(false)}
+                >
+                  <a
+                    className={styles.link}
+                    href={process.env.PUBLIC_URL + "/login"}
+                  >
+                    {t("home-header-login")}
+                  </a>
+                </li>
+                <li
+                  className={styles.accountMenu__item}
+                  onClick={() => setShowAccountMenu(false)}
+                >
+                  <a
+                    className={styles.link}
+                    href={process.env.PUBLIC_URL + "/register"}
+                  >
+                    {t("home-header-register")}
+                  </a>
+                </li>
+                <li
+                  className={styles.accountMenu__item}
+                  onClick={() => setShowAccountMenu(false)}
+                >
+                  <a
+                    className={styles.link}
+                    href={process.env.PUBLIC_URL + "/track-order"}
+                  >
+                    {t("home-header-trackOrder")}
+                  </a>
+                </li>
+              </>
+            )}
+          </ul>
+        ) : null}
       </div>
       <div className={styles.langBox}>
-        <span onClick={showLangHandler} className={styles.selectedLang}>{lang}</span>
-        {
-          showLangMenu ? (
-            <ul className={styles.selectLangList}>
-              <li className={styles.item} onClick={() => chooseLangHandler("ar")}>AR</li>
-              <li className={styles.item} onClick={() => chooseLangHandler("en")}>EN</li>
-            </ul>
-          ) : null
-        }
+        <span onClick={showLangHandler} className={styles.selectedLang}>
+          {lang}
+        </span>
+        {showLangMenu ? (
+          <ul className={styles.selectLangList}>
+            <li className={styles.item} onClick={() => chooseLangHandler("ar")}>
+              AR
+            </li>
+            <li className={styles.item} onClick={() => chooseLangHandler("en")}>
+              EN
+            </li>
+          </ul>
+        ) : null}
       </div>
       {/* <div className={styles.langBox}>
         <select onChange={(e) => i18n.changeLanguage(e.target.value)} className={styles.selectLang}>

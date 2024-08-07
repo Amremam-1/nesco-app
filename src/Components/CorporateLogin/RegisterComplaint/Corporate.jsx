4b339d55c73ef6d081781daff02a3ba66b4da600
Amr Imam <emama2337@gmail.com>
2024-08-08 00:32:06 +0300

    finfih

--- src/Components/CorporateLogin/RegisterComplaint/Corporate.jsx
+++ src/Components/CorporateLogin/RegisterComplaint/Corporate.jsx
@@ -1,19 +1,28 @@
-import ComplaintForm from "../ComplaintForm/ComplaintForm"
+// import ComplaintForm from "../ComplaintForm/ComplaintForm"
 import styles from "./styles.module.scss"
+import { useTranslation } from "react-i18next"
 
 const Corporate = () => {
+  const { t } = useTranslation()
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <header className={styles.header}>
-          <h1>خدمة تقديم البلاغات والشكاوى</h1>
-          <p>
-            نحن هنا لدعمك في تقديم شكاوى عن قهوة نستريتو وفحصها بجدية لضمان
-            الجودة. تواصل معنا لأي استفسارات أو مشكلات.
-          </p>
+          <h1>{t("corporate_title")}</h1>
+          <p>{t("corporate_subtitle")}</p>
         </header>
 
-        <ComplaintForm />
+        {/* <ComplaintForm /> */}
+        <div className={styles.login}>
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSfLmDI0OyxXY8yXYGeQ2V58sEUqgmp0NcMZr5RuY15AVIcXPw/viewform"
+            className={styles.franchising__link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {t("corporate_button")}
+          </a>
+        </div>
       </div>
     </div>
   )
